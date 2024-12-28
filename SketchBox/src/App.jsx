a1f91a68a9e29b7ad7329461bc2637d29318645d
dhara-maru <dharamaru406@gmail.com>
2024-12-28 12:54:26 +0530

    set up Routes of all pages

--- SketchBox/src/App.jsx
+++ SketchBox/src/App.jsx
@@ -1,10 +1,33 @@
+import {Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import Items from './pages/Items';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Cart from './pages/Cart';
+import Login from './pages/Login';
+import PlaceOrder from './pages/PlaceOrder';
+import Product from './pages/Product';
+import Orders from './pages/Orders';
 
 function App() {
   
   return (
-    <>
-     hello
-    </>
+    <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+     
+     <Routes>
+        <Route path='/' element={<Home/>}  />
+        <Route path='/items' element={<Items/>}  />
+        <Route path='/about' element={<About/>}  />
+        <Route path='/contact' element={<Contact/>}  />
+        <Route path='/cart' element={<Cart/>}  />
+        <Route path='/login' element={<Login/>}  />
+        <Route path='/place-order' element={<PlaceOrder/>}  />
+        <Route path='/product/:productId' element={<Product/>}  />
+        <Route path='/orders' element={<Orders/>}  />
+
+     </Routes>
+
+    </div>
   )
 }
 

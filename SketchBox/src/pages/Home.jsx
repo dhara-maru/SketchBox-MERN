7094b5bf905c3f164a89fa9969b12be9b95422e6
dhara-maru <dharamaru406@gmail.com>
2024-12-29 21:59:51 +0530

    added best sellers section

--- SketchBox/src/pages/Home.jsx
+++ SketchBox/src/pages/Home.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import NewItems from '../components/NewItems'
-
+import BestSeller from '../components/BestSeller'
 
 const Home = () => {
   return (
     <div>
       <Hero></Hero>
       <NewItems></NewItems> 
+      <BestSeller />
     </div>
   )
 }

696dfb3c5c7cab8b4d4d4b50683fd19254246461
dhara-maru <dharamaru406@gmail.com>
2025-01-07 19:46:08 +0530

    login / signup page

--- SketchBox/src/pages/Orders.jsx
+++ SketchBox/src/pages/Orders.jsx
@@ -27,8 +27,16 @@ const Orders = () => {
             <p>Quantity : 1</p>
             <p>Type : M</p>
           </div>
-          <p>Date : <span className='text-gray-400'>04 June, 2024</span></p>
+          <p className='mt-2'>Date : <span className='text-gray-400'>04 June, 2024</span></p>
         </div>
+      </div>
+      <div className='md:w-1/2 flex justify-between'>
+      <div className="flex items-center gap-2">
+        <p className='min-w-2 h-2 rounded-full bg-green-500'></p>
+        <p className='text-sm md:text-base'>Ready to Ship</p>
+      </div>
+
+      <button className='border px-4 py-2 text-sm font-medium rounded-sm'>Track Order</button>
       </div>
     </div>
   ))
